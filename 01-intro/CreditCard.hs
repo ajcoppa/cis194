bfff7320047fe4b8324308cdfac070027436d89c
AJ Coppa <aj.coppa@gmail.com>
2014-05-03 12:11:43 -0400

    Finished doubleEveryOther

--- 01-intro/CreditCard.hs
+++ 01-intro/CreditCard.hs
@@ -19,3 +19,14 @@ toDigits n
 -- [4,3,2,1]
 toDigitsRev :: Integer -> [Integer]
 toDigitsRev = reverse . toDigits
+
+-- | Double every other integer in the list, starting from the right.
+--
+-- >>> doubleEveryOther [8,7,6,5]
+-- [16,7,12,5]
+doubleEveryOther :: [Integer] -> [Integer]
+doubleEveryOther xs = helper xs doubleFirstElement
+  where doubleFirstElement = (even (length xs))
+        helper [] _  = []
+        helper (x:xs) True = x * 2 : helper xs False
+        helper (x:xs) False = x : helper xs True
