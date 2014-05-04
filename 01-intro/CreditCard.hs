2b90db5a176d0af7d7e9f8b1ad786c3755be9c7a
AJ Coppa <aj.coppa@gmail.com>
2014-05-04 10:44:41 -0400

    Refactored toDigits and toDigitsRev for performance

--- 01-intro/CreditCard.hs
+++ 01-intro/CreditCard.hs
@@ -1,24 +1,24 @@
 module CreditCard where
 
--- | Convert an Integer to a list of its digits.
---
--- >>> toDigits 1234
--- [1,2,3,4]
--- >>> toDigits 0
--- []
--- >>> toDigits (-17)
--- []
-toDigits :: Integer -> [Integer]
-toDigits n
-  | n <= 0    = []
-  | otherwise = toDigits (n `div` 10) ++ [n `mod` 10]
-
 -- | Convert an Integer to a reversed list of its digits.
 --
 -- >>> toDigitsRev 1234
 -- [4,3,2,1]
+-- >>> toDigitsRev 0
+-- []
+-- >>> toDigitsRev (-17)
+-- []
 toDigitsRev :: Integer -> [Integer]
-toDigitsRev = reverse . toDigits
+toDigitsRev n
+  | n <= 0    = []
+  | otherwise = n `mod` 10 : toDigitsRev (n `div` 10)
+
+-- | Convert an Integer to a list of its digits.
+--
+-- >>> toDigits 1234
+-- [1,2,3,4]
+toDigits :: Integer -> [Integer]
+toDigits = reverse . toDigitsRev
 
 -- | Double every other integer in the list, starting from the right.
 --
