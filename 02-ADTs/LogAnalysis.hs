09013bc871edf697b0b28f2c75e02cb2a3586c7c
AJ Coppa <aj.coppa@gmail.com>
2014-07-05 19:27:38 -0400

    Refactored parseMessage to be much simpler

--- 02-ADTs/LogAnalysis.hs
+++ 02-ADTs/LogAnalysis.hs
@@ -14,46 +14,30 @@ import Control.Applicative
 -- Unknown "This is not in the right format"
 parseMessage :: String -> LogMessage
 parseMessage m =
-  if (< 3) . length . words $ m
-  then Unknown m
-  else
-    let messageWords = words m
-        typeCharacter = messageWords !! 0
-    in case typeCharacter of "E" -> parseErrorMessage messageWords
-                             "I" -> parseInfoMessage messageWords
-                             "W" -> parseWarningMessage messageWords
-                             _ -> Unknown m
+  case words m of
+    ("E":code:timestamp:message) ->
+      parseHelper (Error (read code)) timestamp message
+    ("I":timestamp:message) ->
+      parseHelper Info timestamp message
+    ("W":timestamp:message) ->
+      parseHelper Warning timestamp message
+    _ -> Unknown m
 
--- | Parses an individual log line as an Error message.
---
--- >>> parseErrorMessage ["E","2","562","help","help"]
--- LogMessage (Error 2) 562 "help help"
-parseErrorMessage :: [String] -> LogMessage
-parseErrorMessage ws =
-  let code = read $ ws !! 1
-      timestamp = read $ ws !! 2
-      message = unwords . drop 3 $ ws
-  in LogMessage (Error code) timestamp message
+{- | Parses a MessageType, string representing timestamp, and array of message
+words into a LogMessage.
 
--- | Parses an individual log line as an Info message.
---
--- >>> parseInfoMessage ["I","29","la","la","la"]
--- LogMessage Info 29 "la la la"
-parseInfoMessage :: [String] -> LogMessage
-parseInfoMessage ws =
-  let timestamp = read $ ws !! 1
-      message = unwords . drop 2 $ ws
-  in LogMessage Info timestamp message
+>>> parseHelper (Error 1) "3" ["Pickles","are","too","fuzzy"]
+LogMessage (Error 1) 3 "Pickles are too fuzzy"
 
--- | Parses an individual log line as a Warning message.
---
--- >>> parseWarningMessage ["W","5","Flange","is","due","for","a","check-up"]
--- LogMessage Warning 5 "Flange is due for a check-up"
-parseWarningMessage :: [String] -> LogMessage
-parseWarningMessage ws =
-  let timestamp = read $ ws !! 1
-      message = unwords . drop 2 $ ws
-  in LogMessage Warning timestamp message
+>>> parseHelper Info "8" ["Zoe","is","a","dog"]
+LogMessage Info 8 "Zoe is a dog"
+
+>>> parseHelper Warning "17" ["Nuclear","launch","detected"]
+LogMessage Warning 17 "Nuclear launch detected"
+-}
+parseHelper :: MessageType -> String -> [String] -> LogMessage
+parseHelper msgType timestamp message =
+  LogMessage msgType (read timestamp) (unwords message)
 
 -- | Parses multiple log lines.
 --
